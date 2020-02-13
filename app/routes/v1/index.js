ce5d55c051389f1251caa653c1c387aae4bc70b9
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-02-13 17:25:08 +0900

    ?

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 // ルーティング
 const router = express.Router();
+const request = require('request');
+const config = require('../../../config');
 
-// http://localhost:3000/api/v1/
-router.get('/', function(req, res) {
-  res.json({
-    message: 'Hello,world'
-  });
+// axios を require してインスタンスを生成する
+const axiosBase = require('axios');
+const axios = axiosBase.create({
+  baseURL: 'https://api.trello.com/1/', // バックエンドB のURL:port を指定する
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'
+});
+
+router.get('/', function(req, res, next) {
+  const url = '/boards/' + config.yourBoard + '/lists/';
+  const option = {
+    fields: 'name',
+    key: config.yourApiKey,
+    token: config.oauthToken
+  };
+  axios
+    .get(url, option)
+    .then(function(response) {
+      callback(response);
+    })
+    .catch(function(error) {
+      console.log('ERROR!! occurred in Backend.');
+    });
 });
 
 module.exports = router;
